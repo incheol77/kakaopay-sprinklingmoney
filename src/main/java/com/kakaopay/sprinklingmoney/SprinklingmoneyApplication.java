6ec12eed1f4185556ae6b466f72e1aaad7a1b7ad
incheol77 <incheol77@gmail.com>
2020-12-05 14:42:29 +0900

    Initial commit using spring boot for kakaopay splinkling money APIs

---
+++ src/main/java/com/kakaopay/sprinklingmoney/SprinklingmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.sprinklingmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinklingmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinklingmoneyApplication.class, args);
+	}
+
+}
