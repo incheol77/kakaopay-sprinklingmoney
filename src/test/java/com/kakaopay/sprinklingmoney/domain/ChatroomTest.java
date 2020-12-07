7034c766cc246919cb07266f4659530b2573f086
incheol77 <incheol77@gmail.com>
2020-12-07 12:00:36 +0900

    test : Chatroom.class 에 대한 unit test 추가
    - Chatroom 의 기본 getter, setter 에 대한 unit test 인 ChatroomTest.class 추가

---
+++ src/test/java/com/kakaopay/sprinklingmoney/domain/ChatroomTest.java
@@ -0,0 +1,77 @@
+package com.kakaopay.sprinklingmoney.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatroomTest {
+
+    @Test
+    public void getSetChatroomIdTest() throws Exception {
+        // given
+        Chatroom chatroom = new Chatroom();
+        String testChatroomId = "chatroom-id-123";
+
+        // when
+        chatroom.setChatroomId(testChatroomId);
+
+        // then
+        Assertions.assertThat(testChatroomId).isEqualTo(chatroom.getChatroomId());
+    }
+
+    @Test
+    public void getSetChatRoomNameTest() throws Exception {
+        // given
+        Chatroom chatroom = new Chatroom();
+        String testChatroomName = "참즐거운대화방";
+
+        // when
+        chatroom.setChatroomName(testChatroomName);
+
+        // then
+        Assertions.assertThat(testChatroomName).isEqualTo(chatroom.getChatroomName());
+    }
+
+    @Test
+    public void getSetUserCountTest() throws Exception {
+        // given
+        Chatroom chatroom = new Chatroom();
+        int testUserCount = 100;
+
+        // when
+        chatroom.setUserCount(testUserCount);
+
+        // then
+        Assertions.assertThat(testUserCount).isEqualTo(chatroom.getUserCount());
+    }
+
+    @Test
+    public void getSetUserChatroomsTest() throws Exception {
+        // given
+        Chatroom chatroom = new Chatroom();
+        List<UserChatroom> testUserChatrooms = new ArrayList<>();
+
+        // when
+        chatroom.setUserChatrooms(testUserChatrooms);
+
+        // then
+        Assertions.assertThat(testUserChatrooms).isEqualTo(chatroom.getUserChatrooms());
+    }
+
+
+
+
+
+
+
+}
