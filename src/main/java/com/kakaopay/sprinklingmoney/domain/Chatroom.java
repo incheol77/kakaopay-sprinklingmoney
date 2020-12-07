92de0c0361fb9b5c27f32da97d6553fa3e001d34
incheol77 <incheol77@gmail.com>
2020-12-07 12:12:57 +0900

    docs : 엔티티 속성의 목적에 대한 설명/주석 추가
    - 대화방을 위한 Chatroom.class 에서 문자열 타입의 식별자와 별도로
    대화방의 이름을 위한 chatroomName 속성을 추가한 이유에 대한 설명 추가

--- src/main/java/com/kakaopay/sprinklingmoney/domain/Chatroom.java
+++ src/main/java/com/kakaopay/sprinklingmoney/domain/Chatroom.java
@@ -18,6 +18,11 @@ public class Chatroom {
     @Column(name = "chatroom_id")
     private String chatroomId;
 
+    // 식별자인 chatroomID 가 String 타입이지만 '대화방명'을 식별자로 사용할 경우
+    // 한번 설정한 '대화방 제목'을 변경하기 어려우므로 (식별자 변경...거대한 고통의 시작...ㅠㅠ)
+    // 별도의 '대화방명(chatroomName)' 을 두어 자유롭게 대화방명을 변경할 수 있게 한다.
+    // - 본 과제의 요건에 포함되어 있지는 않으나 매우 일반적인 대화방의 기능이므로 추가함
+    // written by ickim 2020.12.07
     private String chatroomName;
 
     private int userCount;
